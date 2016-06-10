7463e40227f7941ed333480f07da0075c023dc5e
Adagal <jtd@adagal.es>
2016-06-10 11:09:29 +0200

    Plantilla básica para aplicacions angular js con angular ui router

---
+++ PlantillaAngular/app/routes/routes.js
@@ -0,0 +1,18 @@
+﻿app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'app/templates/partial-home.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
+        
+});
